4fb4e718158af4806286f54c1379d17b3e93c623
Rainie <xiaolei.mo@hotmail.com>
2021-06-17 15:04:11 +1000

    17-06-2021

---
+++ initial/src/main/java/com/rainie/restservice/Country.java
@@ -0,0 +1,23 @@
+package com.rainie.restservice;
+
+public class Country {
+
+    private String country_id;
+    private double probability;
+
+    public String getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(String country_id) {
+        this.country_id = country_id;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(int probability) {
+        this.probability = probability;
+    }
+}
